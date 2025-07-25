2a720031fe1e706cdd399dbbfab8e1001b76a799
COMPAORE <fabricecompaore421@gmail.com>
2025-07-25 22:17:26 +0000

    Create bac.vue

---
+++ pages/quizzes/bac.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="min-h-screen p-6">
+    <h1 class="text-2xl font-bold text-blue-700 text-center mb-4">📘 BAC — Quiz</h1>
+    <QuizSerie niveau="BAC" />
+  </div>
+</template>
+
+<script setup>
+import QuizSerie from '~/components/QuizSerie.vue'
+</script>
